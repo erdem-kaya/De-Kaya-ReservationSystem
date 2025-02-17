b539f6c54fe1f508a9c58277f26d4267ebd673ae
Erdem Kaya <kerdem@live.com>
2025-02-17 15:07:24 +0100

    Interfaces for Repositories

---
+++ Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Data.Interfaces;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? expression = null);
+    Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression);
+    Task<TEntity> CreateAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(int id, TEntity updatedEntity);
+    Task<bool> DeleteAsyncById(int id);
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+    Task<bool> ExistsByIdAsync(int id);
+
+}
---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,11 @@
+﻿using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public class BaseRepository<TEntity>(DataContext context) where TEntity : class
+{
+    private readonly DataContext _context = context;
+    private readonly DbSet<TEntity> _dbSet = context.Set<TEntity>();
+
+}
