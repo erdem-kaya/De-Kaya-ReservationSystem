5efc391bbff6fed0d23c484f800b7cc480d27418
Erdem Kaya <kerdem@live.com>
2025-02-17 20:44:41 +0100

    Special Task for Payment and Customer

---
+++ Data/Interfaces/IBookingRepository.cs
@@ -0,0 +1,10 @@
+﻿using Data.Entities;
+
+namespace Data.Interfaces;
+
+public interface IBookingRepository : IBaseRepository<BookingsEntity>
+{
+    // Musterı adına göre rezervasyonları getirir.
+    // Get bookings by customer name.
+    Task<IEnumerable<BookingsEntity>> GetBookingByCustomerName(string username); 
+}
---
+++ Data/Interfaces/IPaymentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.Entities;
+
+namespace Data.Interfaces;
+
+public interface IPaymentRepository : IBaseRepository<PaymentsEntity>
+{
+    // Toplam geliri getirir.
+    // Get total revenue.
+    Task<decimal> GetTotalRevenueAsync();
+
+    // Rezervasyon ID'sine göre ödemeleri getirir.
+    // Get payments by booking ID.
+    Task<IEnumerable<PaymentsEntity>> GetPaymentsByBookingIdAsync(int bookingId); 
+}
