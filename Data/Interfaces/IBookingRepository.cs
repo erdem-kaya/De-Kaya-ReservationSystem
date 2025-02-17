74d876607161fdca364528e43a4cb9ad00e3caf0
Erdem Kaya <kerdem@live.com>
2025-02-17 21:18:37 +0100

    More special methods for booking

--- Data/Interfaces/IBookingRepository.cs
+++ Data/Interfaces/IBookingRepository.cs
@@ -8,7 +8,15 @@ public interface IBookingRepository : IBaseRepository<BookingsEntity>
     // Get bookings by check-in date.
     Task<IEnumerable<BookingsEntity>> GetByCheckInDateAsync(DateTime checkInDate);
 
+    // CheckOut tarihine göre rezervasyonları getirir.
+    // Get bookings by check-out date.
+    Task<IEnumerable<BookingsEntity>> GetByCheckOutDateAsync(DateTime checkOutDate);
+
     // Belli bir tarih aralığındaki rezervasyonları getirir.
     // Get bookings between a certain date range.
     Task<IEnumerable<BookingsEntity>> GetBookingsBetweenDateAsync(DateTime startDate, DateTime endDate);
+
+    // Ödeme durumuna göre rezervasyonları getirir.
+    // Get bookings by payment status.
+    Task<IEnumerable<BookingsEntity>> GetBookingsByPaymentStatusAsync(int paymentStatusId);
 }
--- Data/Repositories/BookingRepository.cs
+++ Data/Repositories/BookingRepository.cs
@@ -23,6 +23,20 @@ public class BookingRepository(DataContext context) : BaseRepository<BookingsEnt
         }
     }
 
+    public async Task<IEnumerable<BookingsEntity>> GetByCheckOutDateAsync(DateTime checkOutDate)
+    {
+        try
+        {
+            var bookings = await _context.Bookings.Where(x => x.CheckOutDate.Date == checkOutDate.Date).ToListAsync();
+            return bookings;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error getting bookings by check-out date : {ex.Message}");
+            return null!;
+        }
+    }
+
     public async Task<IEnumerable<BookingsEntity>> GetBookingsBetweenDateAsync(DateTime startDate, DateTime endDate)
     {
         try
@@ -35,6 +49,22 @@ public class BookingRepository(DataContext context) : BaseRepository<BookingsEnt
         catch (Exception ex)
         {
             Debug.WriteLine($"Error getting bookings between dates : {ex.Message}");
+            return null!;
+        }
+    }
+
+    public async Task<IEnumerable<BookingsEntity>> GetBookingsByPaymentStatusAsync(int paymentStatusId)
+    {
+        try
+        {
+            var bookings = await _context.Bookings
+                .Where(x => x.Payments.Any(x => x.PaymentStatusId == paymentStatusId))
+                .ToListAsync();
+            return bookings;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error getting bookings by payment status : {ex.Message}");
             return null!;
         }
     }
